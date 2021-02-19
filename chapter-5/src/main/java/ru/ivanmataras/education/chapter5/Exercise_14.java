1e8b20c309fbd2d321fd9556b5bb92f1b49906da
Ivan <imataras@yandex.ru>
2021-02-19 23:20:54 +0300

    fix exercises 14 for chapter 5

--- chapter-5/src/main/java/ru/ivanmataras/education/chapter5/Exercise_14.java
+++ chapter-5/src/main/java/ru/ivanmataras/education/chapter5/Exercise_14.java
@@ -2,8 +2,8 @@ package ru.ivanmataras.education.chapter5;
 
 public class Exercise_14 {
 
-    private static String stringField_1 = new String("stringField_1");
-    private static String stringField_2;
+    static String stringField_1 = new String("stringField_1");
+    static String stringField_2;
 
     static {
         stringField_2 = new String("stringField_2");
