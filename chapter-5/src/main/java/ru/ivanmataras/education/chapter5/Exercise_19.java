e9cdb197be0c0171fe62d1b082217a13daf85dde
Ivan <imataras@yandex.ru>
2021-02-21 23:56:02 +0300

    add exercises 19 for chapter 5

---
+++ chapter-5/src/main/java/ru/ivanmataras/education/chapter5/Exercise_19.java
@@ -0,0 +1,20 @@
+package ru.ivanmataras.education.chapter5;
+
+import java.util.Arrays;
+import java.util.UUID;
+
+import static java.lang.System.out;
+
+public class Exercise_19 {
+
+    Exercise_19() {
+
+    }
+
+    void printVariableArguments(String... arguments) {
+        for(String argument : arguments) {
+            out.println(argument);
+        }
+    }
+
+}
---
+++ chapter-5/src/test/java/ru/ivanmataras/education/chapter5/TestExercise_19.java
@@ -0,0 +1,23 @@
+package ru.ivanmataras.education.chapter5;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class TestExercise_19 {
+
+    @Test
+    @Order(1)
+    public void testExercise_19() {
+        Exercise_19 exercise_19 = new Exercise_19();
+        assertNotNull(exercise_19);
+        exercise_19.printVariableArguments(new String("argument1"), new String("argument2"));
+        exercise_19.printVariableArguments(new String[]{("argument1InArray"), new String("argument2InArray")});
+
+    }
+
+}
