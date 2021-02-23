090d010f4cca1498df702e52c982109977a63487
Ivan <imataras@yandex.ru>
2021-02-23 17:53:34 +0300

    fix classes public access for chapter 6

--- chapter-6/src/test/java/ru/ivanmataras/education/chapter6/TestExercise_1.java
+++ chapter-6/src/test/java/ru/ivanmataras/education/chapter6/TestExercise_1.java
@@ -8,11 +8,11 @@ import org.junit.jupiter.api.TestMethodOrder;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class TestExercise_1 {
+class TestExercise_1 {
 
     @Test
     @Order(1)
-    public void testExercise_22() {
+    void testExercise_22() {
         ru.ivanmataras.education.chapter6.package1.Exercise_1 exercise_1_package_1 = new ru.ivanmataras.education.chapter6.package1.Exercise_1();
         assertNotNull(exercise_1_package_1);
         ru.ivanmataras.education.chapter6.package2.Exercise_1 exercise_1_package_2 = new ru.ivanmataras.education.chapter6.package2.Exercise_1();
--- chapter-6/src/test/java/ru/ivanmataras/education/chapter6/TestInitialize1.java
+++ chapter-6/src/test/java/ru/ivanmataras/education/chapter6/TestInitialize1.java
@@ -6,11 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class TestInitialize1 {
+class TestInitialize1 {
 
     @Test
     @Order(1)
-    public void testInitializeClasses1() {
+    void testInitializeClasses1() {
 
     }
 
