f9b804f426b5f5cee00abd8f6dccfd564ae1fe63
Ivan <imataras@yandex.ru>
2021-02-23 22:53:21 +0300

    add exercises 4 for chapter 6

---
+++ chapter-6/src/main/java/ru/ivanmataras/education/chapter6/package1/Exercise_4.java
@@ -0,0 +1,19 @@
+package ru.ivanmataras.education.chapter6.package1;
+
+public class Exercise_4 {
+
+    protected String stringField;
+
+    public Exercise_4() {
+
+    }
+
+    protected String getStringField() {
+        return stringField;
+    }
+
+    protected void setStringField(String stringField) {
+        this.stringField = stringField;
+    }
+
+}
---
+++ chapter-6/src/test/java/ru/ivanmataras/education/chapter6/TestExercise_4.java
@@ -0,0 +1,24 @@
+package ru.ivanmataras.education.chapter6;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import ru.ivanmataras.education.chapter6.package1.Exercise_4;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class TestExercise_4 {
+
+    @Test
+    @Order(1)
+    void testExercise_4() {
+        Exercise_4 exercise_4 = new Exercise_4();
+        assertNotNull(exercise_4);
+        //cant access class method from other package, because method access is protected
+//        exercise_4.getStringField();
+//        exercise_4.setStringField();
+    }
+
+}
