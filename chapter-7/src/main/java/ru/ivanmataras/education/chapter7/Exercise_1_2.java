3e2b16ecc6441892bf9714efe018834bda44b02f
Ivan <imataras@yandex.ru>
2021-03-08 21:11:14 +0300

    fix access

--- chapter-7/src/main/java/ru/ivanmataras/education/chapter7/Exercise_1_2.java
+++ chapter-7/src/main/java/ru/ivanmataras/education/chapter7/Exercise_1_2.java
@@ -2,7 +2,7 @@ package ru.ivanmataras.education.chapter7;
 
 public class Exercise_1_2 {
 
-    public Exercise_1_2() {
+    Exercise_1_2() {
 
     }
 
--- chapter-7/src/main/java/ru/ivanmataras/education/chapter7/Exercise_7_3.java
+++ chapter-7/src/main/java/ru/ivanmataras/education/chapter7/Exercise_7_3.java
@@ -4,7 +4,7 @@ class Exercise_7_3 extends Exercise_7_1 {
 
     private final Exercise_7_2 exercise_5_2 = new Exercise_7_2(2);
 
-    public Exercise_7_3() {
+    Exercise_7_3() {
         super(1);
     }
 
