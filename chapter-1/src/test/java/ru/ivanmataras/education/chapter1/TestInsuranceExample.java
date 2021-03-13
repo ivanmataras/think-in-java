a21e836d68bad8a3c36bbcde5a63c0b99c93a47a
Ivan <imataras@yandex.ru>
2021-03-13 20:41:29 +0300

    add insurance example test

---
+++ chapter-1/src/test/java/ru/ivanmataras/education/chapter1/TestInsuranceExample.java
@@ -0,0 +1,38 @@
+package ru.ivanmataras.education.chapter1;
+
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import static java.lang.System.out;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestMethodOrder(OrderAnnotation.class)
+class TestInsuranceExample {
+
+    @Test
+    @Order(1)
+    void testInsuranceExample() {
+        Insurance insurance = new CarInsurance();
+        int insurancePremium = insurance.premium();
+        String insuranceCategory = Insurance.category();
+
+        out.println("Insurance premium : " + insurancePremium);
+        out.println("Insurance category : " + insuranceCategory);
+
+        assertEquals(200, insurancePremium);
+        assertEquals("Insurance", insuranceCategory);
+
+        CarInsurance carInsurance = new CarInsurance();
+        int carInsurancePremium = carInsurance.premium();
+        String carInsuranceCategory = CarInsurance.category();
+
+        out.println("CarInsurance premium : " + carInsurancePremium);
+        out.println("CarInsurance category : " + carInsuranceCategory);
+
+        assertEquals(200, carInsurancePremium);
+        assertEquals("Car Insurance", carInsuranceCategory);
+    }
+
+}
